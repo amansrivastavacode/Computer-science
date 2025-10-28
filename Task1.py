d39c086dcb7fc03a358294953b863b28a58efc87
amansrivastavacode <amansrivastava988935@gmail.com>
2025-10-28 10:56:11 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+print("Enter the first number:6")
+print("Enter the second number:4")
+print("Addition=",6+4)
+print("Subtraction=",6-4)
+print("Multiplication=",6*4)
+print("Division=",6/4)
+Name=input("Enter your first name:")
+Name=input("Enter your last name:")
+
