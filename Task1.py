6bbb5c1567768da42e7dc1f3975f0de8fe0fa754
amansrivastavacode <amansrivastava988935@gmail.com>
2025-11-27 10:48:19 +0530

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,6 +1,11 @@
-print("Enter the first number:6")
-print("Enter the second number:4")
-print("Addition=",6+4)
-print("Subtraction=",6-4)
-print("Multiplication=",6*4)
-print("Division=",6/4)
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f"addition: {addition}")
+print(f"subtraction: {subtraction}")
+print(f"multiplication: {multiplication}")
+print(f"division: {division}")
+
